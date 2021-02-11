8affdea9cf6ce0889909ca429adad1d372a9036a
gabriel <gabriel.rossi@fattoriaweb.com.br>
2021-02-10 23:51:02 -0300

    scaneando portas de forma concorrente utilizando WaitGroup

--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 func main() {
+
+	var WaitGroup sync.WaitGroup
+
     for i := 1; i <= 1024; i++ {
-		endereco := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conexao, erro := net.Dial("tcp", endereco)
 
-		if erro != nil {
-			//porta fechada ou filtrada
-			continue
-		}
+		WaitGroup.Add(1)
 
-		conexao.Close()
-        fmt.Printf("porta %d aberta\n", i)
+		go func(j int) {
+			
+			defer WaitGroup.Done()
+
+			endereco := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conexao, erro := net.Dial("tcp", endereco)
+	
+			if erro != nil {
+				//porta fechada ou filtrada
+				return
+			}
+	
+			conexao.Close()
+			fmt.Printf("porta %d aberta\n", j)
+		} (i)
+		
+		WaitGroup.Wait()
     }
 }
